4b86fdd493912cab0fdbfd1e0d313530fca92cc0
LavrovNick <lavrovmykola@gmail.com>
2024-05-23 06:59:48 +0300

    minor improvements

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import axios from "axios";
-const port = process.env.PORT || 3001;
 
+const DEFAULT_IMAGE_WIDTH = 640;
+const DEFAULT_IMAGE_HEIGHT = 480;
+
+const port = process.env.PORT || 3001;
 const app = express();
 
 const INTERNAL_SERVICE_ERROR = "Internal service error";
@@ -20,8 +23,7 @@ app.post("/api/v1/prompt", async (req, res) => {
       res.status(400).json({ error: PROMPT_IS_REQUIRED });
       return;
     }
-    const { prompt } = body;
-    const { size } = body;
+    const { prompt, size } = body;
     const imageUrl = getImageUrl(prompt, size);
     console.log({ prompt, imageUrl });
     const imageIsReady = await waitUntilImageIsReady(imageUrl);
@@ -42,7 +44,7 @@ app.listen(port, () => console.log(`get-image-back on port: ${port}`));
 
 //-------------------------------------------------Utils-------------------------------------------------//
 
-function getImageUrl(prompt, size = { width: 320, height: 240 }) {
+function getImageUrl(prompt, size = { width: DEFAULT_IMAGE_WIDTH, height: DEFAULT_IMAGE_HEIGHT }) {
   const encodedPrompt = encodeURIComponent(prompt);
   return `https://image.pollinations.ai/prompt/${encodedPrompt}?width=${size?.width}&height=${size?.height}&nologo=poll&nofeed=yes`;
 }
